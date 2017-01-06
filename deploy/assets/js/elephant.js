eab90f35df39046b5216294f700b16ad77fe3152
Peter Landwehr <plandwehr@gmail.com>
2017-01-05 22:54:25 -0800

    springroll/phaser/html version WIP

---
+++ deploy/assets/js/elephant.js
@@ -0,0 +1,127 @@
+//////////////// GlOBALS
+////////////////
+
+var g_WIDTH = 1024;
+var g_HEIGHT = 768;
+
+var util = {};
+var g_STATES = ["title","levelselect","explore","clue","search"];
+var g_LEVELS = {
+	"EASY": [
+				[["forest-mountain", "hill-mountain"], ["desert-forest", "desert-hill"]]
+			]
+};
+
+//////////////// UTILITY
+////////////////
+
+util.player = (function() {
+	var currentplayer = 1;
+
+	var setPlayerImg_ = function() {
+		jQuery("#playerIcon").html("<img style='width:100%;height:100%' src='assets/images/icon_p"+currentplayer+".png'>");
+	};
+	var togglePlayer_ = function() {
+		currentplayer = currentplayer === 1 ? 2 : 1;
+		setPlayerImg_();
+	};
+	var getPlayer_ = function() {
+		return currentplayer;
+	};
+	var setPlayer_ = function(playerNum) {
+		currentplayer = playerNum;
+		setPlayerImg_();
+	};
+
+	return {
+		"togglePlayer":togglePlayer_,
+		"getPlayer": getPlayer_,
+		"setPlayer": setPlayer_
+	};
+})();
+
+util.template = (function() {
+	var getHTML_ = function(cURL, callback, dataType) {	
+		var response_ = function(data,status,xhr) {
+			if(status === "success" || status === "notmodified") {
+				callback(data);
+			} else {
+				console.log("request failure: " + cURL);
+			}
+		};
+		if(dataType === undefined) {dataType = "text"};
+		jQuery.get(cURL,undefined,response_,dataType);
+	};
+
+	var setDOM_ = function(cURL, elementId) {
+		getHTML_(cURL, function(data){
+			jQuery("#"+elementId).html(data);
+		});
+	};
+
+	return {
+		"getHTML": getHTML_,
+		"setDOM": setDOM_
+	};
+})();
+
+util.triangle = function() {
+	//
+};
+
+//////////////// MISC
+////////////////
+
+var createExploreMap = function(difficulty,level) {
+	if(difficulty === undefined){difficulty="EASY"}
+	if(level===undefined){level=0}
+
+	var activeTile = [0,1]; //y,x
+	var cLevel = g_LEVELS[difficulty][0]; //set current level data
+	var htmlout = "<div style='font-size:0px;display:inline-block;position:absolute;top:0px;left:0px;'>";
+	for(var indy = 0; indy < cLevel.length; indy++) {
+		for(var indx = 0; indx < cLevel[indy].length; indx++) {
+			htmlout += "<img style='display:inline-block' src='assets/images/level1-easy/"+cLevel[indy][indx]+".gif'>";
+		}
+	}
+	htmlout += "</div>";
+	//overlay
+	htmlout += "<table style='width:100%;height:100%;font-size:0px;display:inline-block;position:absolute;top:0px;left:0px;'>";
+	for(var indy = 0; indy < cLevel.length; indy++) {
+		htmlout += "<tr>";
+		for(var indx = 0; indx < cLevel[indy].length; indx++) {
+			htmlout += "<td style='width:475px;height:310px;" + (indy == activeTile[0] && indx == activeTile[1] ? "background-color:rgba(0,0,0,0.30)" : "") + "'>"+"</td>";
+		}
+		htmlout += "</tr>";
+	}
+	htmlout += "</table>";
+	jQuery("#exploremap").html(htmlout);
+};
+
+//////////////// State Transitions
+////////////////
+
+var setStateTitle = function() {
+	util.template.getHTML("assets/js/title.html", function(data){
+		jQuery("#uiLayer").html(data);
+		//init here
+		jQuery("#titletext").click(function(){setStateExplore();});
+	});
+};
+var setStateLevelSelect = function() {
+	//
+};
+var setStateExplore = function() {
+	util.template.getHTML("assets/js/explore.html", function(data){
+		jQuery("#uiLayer").html(data);
+		//init here
+		util.player.setPlayer(1);
+		createExploreMap();
+	});
+};
+var setStateClue = function() {
+	//
+};
+var setStateSearch = function() {
+	//
+};
---
+++ deploy/assets/js/main.js
@@ -0,0 +1,67 @@
+(function(){
+
+	// Library depencencies
+	var Application = include('springroll.Application'),
+		Game = include('Phaser.Game'),
+		Phaser = include('Phaser');
+
+	// Create a new application
+	var app = new Application({
+		name: "phaserelephant",
+		configPath: "assets/config/config.json"
+	});
+
+	// The Phaser game
+	var game;
+
+	// Handle when app is ready to use
+	app.on('init', function()
+	{
+		// Start application
+		this.game = game = new Game(
+			1024,
+			768,
+			Phaser.CANVAS,
+			"content",
+			{
+				preload: preload,
+				create: create
+			},
+			true
+		);
+	});
+
+	// Preload assets for Phaser
+	function preload()
+	{
+		game.load.image('logo', 'assets/images/logo.png');
+	}
+
+	// When the Phaser game has been created
+	function create()
+	{
+		//game.stage.backgroundColor = "rgb(255,255,255)";
+		var logo = game.add.sprite(
+			game.world.centerX, 
+			game.world.centerY, 
+			'logo'
+		);
+		logo.anchor.setTo(0.5, 0.5);
+
+		var uiHTML = "<div id='uiLayer'></div>";
+		if(jQuery("#uiLayer").length === 0) {
+			jQuery("#content").prepend(uiHTML);
+			jQuery("#uiLayer").addClass("uiLayer");
+		}
+		jQuery("canvas").addClass("gameCanvas");
+		setStateTitle(); //set state to title screen
+	}
+
+	function update() {
+		//
+	}
+
+	// Assign to the window for easy access
+	window.app = app;
+	
+}());
