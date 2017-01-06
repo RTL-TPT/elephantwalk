e1d264d372a7517ca2975e91648696b80a39ae9b
Peter Landwehr <plandwehr@gmail.com>
2017-01-06 10:32:24 -0800

    Add first person view and rotation to exploration state

--- deploy/assets/js/elephant.js
+++ deploy/assets/js/elephant.js
@@ -11,6 +11,9 @@ var g_LEVELS = {
 				[["forest-mountain", "hill-mountain"], ["desert-forest", "desert-hill"]]
 			]
 };
+var g_heading = "north";
+var g_activeTile = [0,1];
+var g_activeLevel = g_LEVELS["EASY"][0];
 
 //////////////// UTILITY
 ////////////////
@@ -76,9 +79,10 @@ var createExploreMap = function(difficulty,level) {
 	if(difficulty === undefined){difficulty="EASY"}
 	if(level===undefined){level=0}
 
-	var activeTile = [0,1]; //y,x
+	//var activeTile = [0,1]; //y,x
 	var cLevel = g_LEVELS[difficulty][0]; //set current level data
-	var htmlout = "<div style='font-size:0px;display:inline-block;position:absolute;top:0px;left:0px;'>";
+	g_activeLevel = cLevel;
+	var htmlout = "<div id='mapGrid' class='mapGrid'>";
 	for(var indy = 0; indy < cLevel.length; indy++) {
 		for(var indx = 0; indx < cLevel[indy].length; indx++) {
 			htmlout += "<img style='display:inline-block' src='assets/images/level1-easy/"+cLevel[indy][indx]+".gif'>";
@@ -86,16 +90,47 @@ var createExploreMap = function(difficulty,level) {
 	}
 	htmlout += "</div>";
 	//overlay
-	htmlout += "<table style='width:100%;height:100%;font-size:0px;display:inline-block;position:absolute;top:0px;left:0px;'>";
+	htmlout += "<table id='mapGridOverlay' class='mapGridOverlay'>";
 	for(var indy = 0; indy < cLevel.length; indy++) {
 		htmlout += "<tr>";
 		for(var indx = 0; indx < cLevel[indy].length; indx++) {
-			htmlout += "<td style='width:475px;height:310px;" + (indy == activeTile[0] && indx == activeTile[1] ? "background-color:rgba(0,0,0,0.30)" : "") + "'>"+"</td>";
+			htmlout += "<td class='"+ (indy == g_activeTile[0] && indx == g_activeTile[1] ? "activeTile" : "") 
+				+"' style='width:475px;height:310px;" 
+				+ (indy == g_activeTile[0] && indx == g_activeTile[1] ? "background-color:rgba(0,0,0,0.30)" : "") 
+				+ "'>"+"</td>";
 		}
 		htmlout += "</tr>";
 	}
 	htmlout += "</table>";
+	htmlout += "<div id='firstPerson' class='firstPerson'></div>";
 	jQuery("#exploremap").html(htmlout);
+	jQuery(".activeTile").click(function(){
+		jQuery("#firstPerson").html("<img style='display:inline-block' src='assets/images/level1-easy/"+cLevel[g_activeTile[0]][g_activeTile[1]]+"/"+g_heading+".jpg'>");
+		jQuery(".arrow").show();
+		jQuery("#rightArrow").unbind().click(function(){rotateView("right")});
+		jQuery("#leftArrow").unbind().click(function(){rotateView("left")});
+	});
+};
+
+var rotateView = function(direction) {
+	if(direction === undefined) {
+		direction = "right"
+	}
+	switch(g_heading) {
+		case "north":
+			g_heading = direction === "right" ? "east" : "west";
+			break;
+		case "east":
+			g_heading = direction === "right" ? "south" : "north";
+			break;
+		case "south":
+			g_heading = direction === "right" ? "west" : "east";
+			break;
+		case "west":
+			g_heading = direction === "right" ? "north" : "south";
+			break;
+	}
+	jQuery("#firstPerson").html("<img style='display:inline-block' src='assets/images/level1-easy/"+g_activeLevel[g_activeTile[0]][g_activeTile[1]]+"/"+g_heading+".jpg'>");
 };
 
 //////////////// State Transitions
