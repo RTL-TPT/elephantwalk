557bee393ef406c6c90758187f58d5a80ddb6e4d
Peter Landwehr <plandwehr@gmail.com>
2017-01-23 13:38:44 -0800

    Search view GPS

--- deploy/assets/js/elephant.js
+++ deploy/assets/js/elephant.js
@@ -256,7 +256,7 @@ var createExploreMap = function() {
 		}
 	}
 	htmlout += "</div>";
-	//map "fog" layer
+	//highlight active block
 	htmlout += "<div id='mapGridOverlay' class='mapGridOverlay'>";
 	for(indy = 0; indy < g_activeLevel.length; indy++) {
 		for(indx = 0; indx < g_activeLevel[indy].length; indx++) {
@@ -294,7 +294,7 @@ var createExploreGPS = function() {
 		}
 	}
 	htmlout += "</div>";
-	//map "fog" layer
+	//highlight active block
 	htmlout += "<div id='' class='mapGPSOverlay'>";
 	for(indy = 0; indy < g_activeLevel.length; indy++) {
 		for(indx = 0; indx < g_activeLevel[indy].length; indx++) {
@@ -307,10 +307,10 @@ var createExploreGPS = function() {
 	htmlout += "<div id='' class='mapGPSOverlay'>";
 	for(indy = 0; indy < g_activeLevel.length; indy++) {
 		for(indx = 0; indx < g_activeLevel[indy].length; indx++) {
-			var borderright = indx < g_activeLevel[indy].length - 1 ? "border-right: 1px dashed black;" : "";
-			var borderleft = indx > 0 ? "border-left: 1px dashed black;" : "";
-			var borderup = indy > 0 ? "border-top: 1px dashed black;" : "";
-			var borderdown = indy < g_activeLevel.length - 1 ? "border-bottom: 1px dashed black;" : "";
+			var borderright = indx < g_activeLevel[indy].length - 1 ? "border-right: 2px dashed black;" : "";
+			var borderleft = indx > 0 ? "border-left: 2px dashed black;" : "";
+			var borderup = indy > 0 ? "border-top: 2px dashed black;" : "";
+			var borderdown = indy < g_activeLevel.length - 1 ? "border-bottom: 2px dashed black;" : "";
 			var borderSum = borderright + borderleft + borderup + borderdown;
 			var cTileIsActive = g_activeTile[0] == indy && g_activeTile[1] == indx;
 			htmlout += "<div style='display:inline-block;box-sizing:border-box;width:475px;height:310px;"+borderSum+"' class='"+(cTileIsActive ? "gps-arrow-"+g_heading : "")+"'></div>";
@@ -320,8 +320,46 @@ var createExploreGPS = function() {
 	jQuery("#firstPerson").append(htmlout);
 };
 
+var createSearchGPS = function() {
+	//map feature layer
+	var htmlout = "<div id='gpsmap' class='GPSContainer'><div id='' class='mapGPS'>";
+	for(var indy = 0; indy < g_activeLevel.length; indy++) {
+		for(var indx = 0; indx < g_activeLevel[indy].length; indx++) {
+			var cTileIsActive = g_activeTile[0] == indy && g_activeTile[1] == indx;
+			var cTileIsVisited = g_tilesRemaining[""+indy+","+indx] === undefined;
+			htmlout += "<img class='exploreMapImg' style='display:inline-block;"+";' coordinant='"+indy+"_"+indx+"' src='assets/images/level1-easy/"+g_activeLevel[indy][indx]+".gif'>";
+		}
+	}
+	htmlout += "</div>";
+	//highlight active block
+	htmlout += "<div id='' class='mapGPSOverlay'>";
+	for(indy = 0; indy < g_activeLevel.length; indy++) {
+		for(indx = 0; indx < g_activeLevel[indy].length; indx++) {
+			htmlout += "<div class='"+ (indy == g_activeTile[0] && indx == g_activeTile[1] ? "activeTile" : "") 
+				+"' style='display:inline-block;width:475px;height:310px;"+(indy == g_activeTile[0] && indx == g_activeTile[1] ? "background-color:rgba(255,255,0,0.30)" : "")+"'></div>";
+		}
+	}
+	htmlout += "</div>";
+	//map grid layer (dotted-lines) + pointer arrow
+	htmlout += "<div id='' class='mapGPSOverlay'>";
+	for(indy = 0; indy < g_activeLevel.length; indy++) {
+		for(indx = 0; indx < g_activeLevel[indy].length; indx++) {
+			var borderright = indx < g_activeLevel[indy].length - 1 ? "border-right: 2px dashed black;" : "";
+			var borderleft = indx > 0 ? "border-left: 2px dashed black;" : "";
+			var borderup = indy > 0 ? "border-top: 2px dashed black;" : "";
+			var borderdown = indy < g_activeLevel.length - 1 ? "border-bottom: 2px dashed black;" : "";
+			var borderSum = borderright + borderleft + borderup + borderdown;
+			var cTileIsActive = g_activeTile[0] == indy && g_activeTile[1] == indx;
+			htmlout += "<div style='display:inline-block;box-sizing:border-box;width:475px;height:310px;"+borderSum+"' class='"+(cTileIsActive ? "gps-arrow-"+g_heading : "")+"'></div>";
+		}
+	}
+	htmlout += "</div></div>";
+	jQuery("#exploremap").append(htmlout);
+};
+
 var createSearchView = function() {
 	jQuery("#exploremap").html("<img style='display:inline-block' src='assets/images/level1-easy/"+g_activeLevel[g_activeTile[0]][g_activeTile[1]]+"/"+g_heading+".jpg'>");
+	createSearchGPS();
 	jQuery(".arrow").show();
 	jQuery("#rightArrow").unbind().click(function(){searchRotate("right")});
 	jQuery("#leftArrow").unbind().click(function(){searchRotate("left")});
@@ -367,9 +405,11 @@ var searchRotate = function(direction) {
 	}
 	g_directionsRemaining = g_directionsRemaining.replace(g_heading[0], "");
 	jQuery("#exploremap").html("<img style='display:inline-block' src='assets/images/level1-easy/"+g_activeLevel[g_activeTile[0]][g_activeTile[1]]+"/"+g_heading+".jpg'>");
+	createSearchGPS();
 	var cElephant = g_LEVEL_ELEPHANT[g_selectedDifficulty][g_selectedLevel];
 	if(cElephant[0] == g_activeTile[0] && cElephant[1] == g_activeTile[1] && cElephant[2] == g_heading) {
 		jQuery("#exploremap").html("<img style='display:inline-block' src='assets/images/level1-easy/"+g_activeLevel[g_activeTile[0]][g_activeTile[1]]+"/"+g_heading+"-elephant.jpg'>");
+		createSearchGPS();
 		setTimeout(function(){
 			alert("You found the elephant!");
 			setStateTitle();
