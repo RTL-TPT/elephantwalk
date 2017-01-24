c466080dc4e6b943815042889e01be0282718f0a
Peter Landwehr <plandwehr@gmail.com>
2017-01-24 15:32:01 -0800

    Update land maps data based on docs

--- deploy/assets/js/jsondata.js
+++ deploy/assets/js/jsondata.js
@@ -1,43 +1,423 @@
 var g_leveldata =
 {
-	"LAND": {
-		"TUTORIAL": [
-			{"clues":["desert","forest"],"symbolStyle":"partialAbstract","clueUnlock":"forest1","gridSize":"2x2","elephantLocation":[1,0,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}},
-			{"clues":["hill","mountain"],"symbolStyle":"partialAbstract","clueUnlock":"mountain1","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}}
-		],
-		"EASY": [
-			{"clues":["forest","hill"],"symbolStyle":"partialAbstract","clueUnlock":"hill1","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}},
-			{"clues":["mountain","desert"],"symbolStyle":"partialAbstract","clueUnlock":"desert1","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}}
-		],
-		"MEDIUM": [
-			{"clues":["desert","hill"],"symbolStyle":"partialAbstract","clueUnlock":"hill1","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}},
-			{"clues":["forest","desert"],"symbolStyle":"partialAbstract","clueUnlock":"desert1","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}},
-			{"clues":["hill","mountain"],"symbolStyle":"partialAbstract","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}},
-			{"clues":["mountain","forest"],"symbolStyle":"partialAbstract","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}}
-		],
-		"HARD": [
-			{"clues":["desert","hill"],"symbolStyle":"partialAbstract","clueUnlock":"hill1","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}},
-			{"clues":["mountain","desert"],"symbolStyle":"partialAbstract","clueUnlock":"desert1","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}},
-			{"clues":["hill","forest"],"symbolStyle":"partialAbstract","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}},
-			{"clues":["forest","mountain"],"symbolStyle":"partialAbstract","gridSize":"2x2","elephantLocation":[0,1,"south"],"clueLocations":{"forest":[1,0.5],"mountain":[0.5,1],"desert":[1.5,1],"hill":[1,1.5]}}
-		]
-	},
-	"WATER": {
-		"TUTORIAL": {},
-		"EASY": {},
-		"MEDIUM": {},
-		"HARD": {}
-	},
-	"MANMADE": {
-		"TUTORIAL": {},
-		"EASY": {},
-		"MEDIUM": {},
-		"HARD": {}
-	},
-	"MASTER": {
-		"TUTORIAL": {},
-		"EASY": {},
-		"MEDIUM": {},
-		"HARD": {}
-	}
+    "LAND": {
+        "TUTORIAL": [
+            {
+                "clues": [
+                    "desert",
+                    "forest"
+                ],
+                "symbolStyle": "partialAbstract",
+                "clueUnlock": "forest1",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    0,
+                    0,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        0.5,
+                        1
+                    ],
+                    "mountain": [
+                        1.5,
+                        1
+                    ],
+                    "desert": [
+                        1,
+                        0.5
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            },
+            {
+                "clues": [
+                    "hill",
+                    "mountain"
+                ],
+                "symbolStyle": "partialAbstract",
+                "clueUnlock": "mountain1",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    1,
+                    1,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        0.5,
+                        1
+                    ],
+                    "mountain": [
+                        1.5,
+                        1
+                    ],
+                    "desert": [
+                        1,
+                        0.5
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            }
+        ],
+        "EASY": [
+            {
+                "clues": [
+                    "forest",
+                    "hill"
+                ],
+                "symbolStyle": "partialAbstract",
+                "clueUnlock": "hill1",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    0,
+                    1,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        0.5,
+                        1
+                    ],
+                    "mountain": [
+                        1.5,
+                        1
+                    ],
+                    "desert": [
+                        1,
+                        0.5
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            },
+            {
+                "clues": [
+                    "mountain",
+                    "desert"
+                ],
+                "symbolStyle": "partialAbstract",
+                "clueUnlock": "desert1",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    1,
+                    0,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        0.5,
+                        1
+                    ],
+                    "mountain": [
+                        1.5,
+                        1
+                    ],
+                    "desert": [
+                        1,
+                        0.5
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            }
+        ],
+        "MEDIUM": [
+            {
+                "clues": [
+                    "desert",
+                    "hill"
+                ],
+                "symbolStyle": "partialAbstract",
+                "clueUnlock": "hill1",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    1,
+                    1,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        1,
+                        0.5
+                    ],
+                    "mountain": [
+                        0.5,
+                        1
+                    ],
+                    "desert": [
+                        1.5,
+                        1
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            },
+            {
+                "clues": [
+                    "forest",
+                    "desert"
+                ],
+                "symbolStyle": "partialAbstract",
+                "clueUnlock": "desert1",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    1,
+                    0,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        1,
+                        0.5
+                    ],
+                    "mountain": [
+                        0.5,
+                        1
+                    ],
+                    "desert": [
+                        1.5,
+                        1
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            },
+            {
+                "clues": [
+                    "hill",
+                    "mountain"
+                ],
+                "symbolStyle": "partialAbstract",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    0,
+                    1,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        1,
+                        0.5
+                    ],
+                    "mountain": [
+                        0.5,
+                        1
+                    ],
+                    "desert": [
+                        1.5,
+                        1
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            },
+            {
+                "clues": [
+                    "mountain",
+                    "forest"
+                ],
+                "symbolStyle": "partialAbstract",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    0,
+                    0,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        1,
+                        0.5
+                    ],
+                    "mountain": [
+                        0.5,
+                        1
+                    ],
+                    "desert": [
+                        1.5,
+                        1
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            }
+        ],
+        "HARD": [
+            {
+                "clues": [
+                    "desert",
+                    "hill"
+                ],
+                "symbolStyle": "partialAbstract",
+                "clueUnlock": "hill1",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    0,
+                    1,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        1.5,
+                        1
+                    ],
+                    "mountain": [
+                        1,
+                        0.5
+                    ],
+                    "desert": [
+                        0.5,
+                        1
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            },
+            {
+                "clues": [
+                    "mountain",
+                    "desert"
+                ],
+                "symbolStyle": "partialAbstract",
+                "clueUnlock": "desert1",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    0,
+                    0,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        1.5,
+                        1
+                    ],
+                    "mountain": [
+                        1,
+                        0.5
+                    ],
+                    "desert": [
+                        0.5,
+                        1
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            },
+            {
+                "clues": [
+                    "hill",
+                    "forest"
+                ],
+                "symbolStyle": "partialAbstract",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    1,
+                    1,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        1.5,
+                        1
+                    ],
+                    "mountain": [
+                        1,
+                        0.5
+                    ],
+                    "desert": [
+                        0.5,
+                        1
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            },
+            {
+                "clues": [
+                    "forest",
+                    "mountain"
+                ],
+                "symbolStyle": "partialAbstract",
+                "gridSize": "2x2",
+                "elephantLocation": [
+                    1,
+                    0,
+                    "south"
+                ],
+                "clueLocations": {
+                    "forest": [
+                        1.5,
+                        1
+                    ],
+                    "mountain": [
+                        1,
+                        0.5
+                    ],
+                    "desert": [
+                        0.5,
+                        1
+                    ],
+                    "hill": [
+                        1,
+                        1.5
+                    ]
+                },
+                "unlocked":true
+            }
+        ]
+    },
+    "WATER": {
+        "TUTORIAL": [],
+        "EASY": [],
+        "MEDIUM": [],
+        "HARD": []
+    },
+    "MANMADE": {
+        "TUTORIAL": [],
+        "EASY": [],
+        "MEDIUM": [],
+        "HARD": []
+    },
+    "MASTER": {
+        "TUTORIAL": [],
+        "EASY": [],
+        "MEDIUM": [],
+        "HARD": []
+    }
 };
